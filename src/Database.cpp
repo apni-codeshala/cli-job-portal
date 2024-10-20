f032b0d50702c1c885d07d0c8d9c07d27d423dd6
apni-codeshala <abhishekpatel171019@gmail.com>
2024-10-20 07:31:37 +0100

    Initial folder structure

--- src/Database.cpp
+++ src/Database.cpp
@@ -2,7 +2,7 @@
 
 sql::Connection* Database::connect() {
     sql::mysql::MySQL_Driver* driver = sql::mysql::get_mysql_driver_instance();
-    sql::Connection* con = driver->connect("tcp://127.0.0.1:3306", "root", "753691abhi");
+    sql::Connection* con = driver->connect("tcp://127.0.0.1:3306", "root", "*********");
     con->setSchema("JobPortal");
     return con;
 }
